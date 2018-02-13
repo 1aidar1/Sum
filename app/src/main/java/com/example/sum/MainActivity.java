ed8d44fa8b120e7f1b5c12901d26c71a23f38b37
Aidarkhan Arkayev <aidar22222222@gmail.com>
2018-02-13 18:27:02 +0600

    2

--- app/src/main/java/com/example/sum/MainActivity.java
+++ app/src/main/java/com/example/sum/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.sum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        System.out.println("KYKYKY");
+        EditText t = (EditText) findViewById(R.id.editText);
+        EditText t2 = (EditText) findViewById(R.id.editText2);
+        EditText t3 = (EditText) findViewById(R.id.editText3);
+        int a,b,c;
+        try{
+            a=Integer.parseInt(t.getText().toString());
+            b=Integer.parseInt(t2.getText().toString());
+            c=a+b;
+            t3.setText(Integer.toString(c));
+
+        }
+        catch (Exception e){
+            t3.setText("Ошибка");
+        }
     }
 }
